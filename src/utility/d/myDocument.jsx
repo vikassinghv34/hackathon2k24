32e60d1666c02f289cf30d836886020981769d9b
Smit Solanki <2010010619@adit.ac.in>
2024-03-16 18:14:08 +0530

    other components

---
+++ src/utility/d/myDocument.jsx
@@ -0,0 +1,20 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <script src="https://www.google.com/recaptcha/api.js?render=YOUR_RECAPTCHA_SITE_KEY"></script>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
